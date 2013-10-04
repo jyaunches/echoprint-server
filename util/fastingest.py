3ec9e4d6253e16d792afb3feb90cb890e5b6dc5b
jyaunches <jmyaunch@gmail.com>
2013-10-04 13:12:09 -0400

    Julie: printing exceptions

--- util/fastingest.py
+++ util/fastingest.py
@@ -16,32 +16,35 @@ def parse_json_dump(jfile):
     bigeval = {}
     fullcodes = []
     for c in codes:
-        if "code" not in c:
-            continue
-        code = c["code"]
-        m = c["metadata"]
-        if "track_id" in m:
-            trid = m["track_id"].encode("utf-8")
-        else:
-            trid = fp.new_track_id()
-        length = m["duration"]
-        version = m["version"]
-        artist = m.get("artist", None)
-        title = m.get("title", None)
-        release = m.get("release", None)
-        decoded = fp.decode_code_string(code)
-        
-        bigeval[trid] = m
-        
-        data = {"track_id": trid,
-            "fp": decoded,
-            "length": length,
-            "codever": "%.2f" % version
-        }
-        if artist: data["artist"] = artist
-        if release: data["release"] = release
-        if title: data["track"] = title
-        fullcodes.append(data)
+    	try: 
+	        if "code" not in c:
+	            continue
+	        code = c["code"]
+	        m = c["metadata"]
+	        if "track_id" in m:
+	            trid = m["track_id"].encode("utf-8")
+	        else:
+	            trid = fp.new_track_id()
+	        length = m["duration"]
+	        version = m["version"]
+	        artist = m.get("artist", None)
+	        title = m.get("title", None)
+	        release = m.get("release", None)
+	        decoded = fp.decode_code_string(code)
+	        
+	        bigeval[trid] = m
+	        
+	        data = {"track_id": trid,
+	            "fp": decoded,
+	            "length": length,
+	            "codever": "%.2f" % version
+	        }
+	        if artist: data["artist"] = artist
+	        if release: data["release"] = release
+	        if title: data["track"] = title
+	        fullcodes.append(data)
+	    except Exception, e:
+	    	print 'Exception:', e    
 
     return (fullcodes, bigeval)
 
